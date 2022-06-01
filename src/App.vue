34ade3de256c57ea3bfeee965b1196dd1e66d18b
gabrielforster <rochafrgabriel@gmail.com>
2022-06-01 19:48:53 -0300

    feat: add styles folder and reset css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="main-nav">
+    <h2 class="logo" @click="$router.go(-1)">Vue App</h2>
+    <nav class="links">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
   </nav>
   <router-view/>
 </template>
 
+<style lang="scss" src="./styles/reset.scss" />
 <style lang="scss">
-#app {
+html {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.main-nav{
+  height: 5rem;
+  padding-inline: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #d5d5d5dd;
+  box-shadow: #2c3e50 0 0 1px 0;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  .links {
 
-    &.router-link-exact-active {
-      color: #42b983;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
-  }
+}
+
 }
 </style>
